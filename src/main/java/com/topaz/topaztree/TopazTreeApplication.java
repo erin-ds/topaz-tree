29e7507b35e5210a6dc02938c0343117037a5b51
Дмитрий Ерин <erin_90@mail.ru>
2022-05-15 22:12:34 +0300

    add project

---
+++ src/main/java/com/topaz/topaztree/TopazTreeApplication.java
@@ -0,0 +1,13 @@
+package com.topaz.topaztree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopazTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopazTreeApplication.class, args);
+	}
+
+}
